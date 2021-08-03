c7b1b074b7fef690c0be9bcf8fc9e82f8d448b31
Bernhard Millauer <bernhard.millauer@adventics.de>
2021-08-03 10:34:47 +0200

    exclude new download script from package generation

--- build/build.bat
+++ build/build.bat
@@ -5,4 +5,4 @@ set NUGET=%PWD%\NuGet.exe
 
 DEL %PWD%\..\*.nupkg
 
-%NUGET% pack -Verbosity detailed -OutputDirectory %PWD%\.. -Exclude "**/build/**;**/.git/**;**/.git*;**/README.md;**/icon.png" %PWD%\..\WebDriver.ChromeDriver.win32.nuspec
+%NUGET% pack -Verbosity detailed -OutputDirectory %PWD%\.. -Exclude "**/build/**;**/.git/**;**/.git*;**/README.md;**/icon.png;**/download-latest.ps1" %PWD%\..\WebDriver.ChromeDriver.win32.nuspec
